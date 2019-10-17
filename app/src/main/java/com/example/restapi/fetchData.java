0b46fd31fcb2102ce68390615917dabd2b678ff5
ilyas afriyan <you@example.com>
2019-10-17 13:43:31 +0700

    for data json

--- app/src/main/java/com/example/restapi/fetchData.java
+++ app/src/main/java/com/example/restapi/fetchData.java
@@ -1,6 +1,9 @@
 package com.example.restapi;
 
 import android.os.AsyncTask;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,6 +16,8 @@ import java.net.URL;
 public class fetchData extends AsyncTask<Void,Void,Void> {
 
     String data = "";
+    String dataParsed = "";
+    String singleParsed = "";
 
     @Override
     protected Void doInBackground(Void... voids) {
@@ -22,14 +27,20 @@ public class fetchData extends AsyncTask<Void,Void,Void> {
             InputStream inputStream = httpURLConnection.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
             String line = "";
-
             while(line != null){
                 line = bufferedReader.readLine();
                 data = data + line;
             }
+            JSONArray ja = new JSONArray(data);
+            for(int i =0; i<ja.length(); i++){
+                JSONObject jo = (JSONObject) ja.get(i);
+
+            }
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
